f7b19af72ef116b3ce790119731a3b0f2e142514
Juin Chiu <davidjuin0519@gmail.com>
2017-08-01 01:02:03 +0800

    (feat): Implement transaction class and enhance related classes

--- simblock/block.py
+++ simblock/block.py
@@ -1,3 +1,25 @@
 class Block():
-    def __init__(self, header=None):
+    def __init__(self, header=None, transactions=None):
+        if not header:
+            raise ValueError("Argument 'header' is not provided.")
         self.header = header
+        self.transactions = transactions or []
+
+    def from_prevstate(self, state=None, timestamp=None):
+        if not state:
+            raise ValueError("Argument 'state' is not provided.")
+
+        prev_header = state.prev_headers[0]
+        next_header = {
+            "prevhash": prev_header.hash,
+            "timestamp": timestamp or prev_header.timestamp + 1,
+            "difficulty": prev_header.difficulty,
+            "nonce": ""
+        }
+
+        self.header.prevhash = next_header["prevhash"]
+        self.header.timestamp = next_header["timestamp"]
+        self.header.difficulty = next_header["difficulty"]
+        self.header.nonce = next_header["nonce"]
+
+        return self
--- simblock/transaction.py
+++ simblock/transaction.py
@@ -1,18 +1,39 @@
-from simblock import utils
+import pickle
+from .utils import privkey_to_addr, sha3_256, ec_sign
 
 class Transaction():
-    """
-    A simple implementation of transaction referencing Ethereum's.
-    """
+    fields = [
+        "nonce",
+        "to",
+        "value",
+        "data",
+        "v",
+        "r",
+        "s",
+    ]
 
-    def __init__(self, nounce, to, value, v=0, r=0, s=0):
-        # fields = [
-        #     ("nounce", ""),
-        #     ("to", ""),
-        #     ("value", ""),
-        #     ("v", ""),
-        #     ("r", ""),
-        #     ("s", "")
-        # ]
-        to = utils.normalize_address(to)
+    def __init__(self, tx_data=None):
+        if tx_data is None:
+            raise ValueError("Argument 'tx_data' is not provided.")
 
+        self.tx_data = tx_data
+        self.sender = None
+
+        # Signature
+        self.v = 0
+        self.r = 0
+        self.s = 0
+        self.signed = False
+
+    def sign(self, privkey):
+        if self.signed == True:
+            return None
+        else:
+            raw_hash = sha3_256(pickle.dumps(self))
+            self.v, self.r, self.s = ec_sign(raw_hash, privkey)
+            self.signed = True
+
+        # Set sender
+        self.sender = privkey_to_addr(privkey)
+
+        return self
